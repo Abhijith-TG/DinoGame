3cc5dfd4cef682f3dad6264bccb4c7d321c1f504
Abhijith-TG <abhijithtg62@gmail.com>
2024-11-03 19:42:56 +0530

    added game over feature but bugs persist...

--- javaScript/app.js
+++ javaScript/app.js
@@ -1,11 +1,16 @@
 const player = document.getElementById("player");
 const enemy = document.getElementById("enemy");
+const marks = document.getElementById("marks");
+let mark = 0;
+
 
 
 player.addEventListener('keypress',(e)=>{
    if(e.key==" "){
     if(player.className!=="animate"){
         player.classList.add("animate");
+        mark++;
+        marks.innerHTML=mark;
     }
     setTimeout(()=>{
         player.classList.remove("animate");
@@ -39,6 +44,18 @@ start.onclick=()=>{
 document.onclick=()=>{
     player.focus();
 }
+
+const checkDead = setInterval(()=>{
+    let characterTop = parseInt(window.getComputedStyle(player).getPropertyValue("top"));
+    let enemyLeft = parseInt(window.getComputedStyle(enemy).getPropertyValue("left"));
+    
+    if(enemyLeft<20 && enemyLeft>0 && characterTop>=120){
+        console.log(characterTop,enemyLeft);
+        alert("Game Over \n marks = "+mark+ "\n Reload to play again");
+        
+    }
+},10); 
+
 
 
 
